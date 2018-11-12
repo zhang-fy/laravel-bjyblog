1b31f6a4048a4d7ee874b5d2d9d9f6f629a43322
zfy <zhangfengyang@weiche.cn>
2018-11-12 20:16:50 +0800

    修改主页模板

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <link rel="shortcut icon" href="/public/longlongago.ico">
+    <link rel="shortcut icon" href="longlongago.png">
     @yield('css')
 </head>
 <body>

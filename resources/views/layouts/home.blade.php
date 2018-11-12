7a3e49b2bc1ed3662f6675f839e8065ae0c3d34b
zfy <zhangfengyang@weiche.cn>
2018-11-12 15:11:03 +0800

    提交icon图标

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="shortcut icon" href="/public/longlongago.ico">
     @yield('css')
 </head>
 <body>

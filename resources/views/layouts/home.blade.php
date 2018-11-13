43b926b174c3a296e62cf6b5ac193bf8f4ac9616
zfy <zhangfengyang@weiche.cn>
2018-11-13 19:03:19 +0800

    tijiao xiugai

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -210,15 +210,15 @@
             </div>
             <div class="col-xs-12 col-md-12 col-lg-12 b-login-row">
                 <ul class="row">
-                    <li class="col-xs-6 col-md-4 col-lg-4 b-login-img">
-                        <a href="{{ url('auth/oauth/redirectToProvider/qq') }}"><img src="{{ asset('images/home/qq-login.png') }}" alt="QQ登录" title="QQ登录"></a>
-                    </li>
+                    {{--<li class="col-xs-6 col-md-4 col-lg-4 b-login-img">--}}
+                        {{--<a href="{{ url('auth/oauth/redirectToProvider/qq') }}"><img src="{{ asset('images/home/qq-login.png') }}" alt="QQ登录" title="QQ登录"></a>--}}
+                    {{--</li>--}}
                     <li class="col-xs-6 col-md-4 col-lg-4 b-login-img">
                         <a href="{{ url('auth/oauth/redirectToProvider/weibo') }}"><img src="{{ asset('images/home/sina-login.png') }}" alt="微博登录" title="微博登录"></a>
                     </li>
-                    <li class="col-xs-6 col-md-4 col-lg-4 b-login-img">
-                        <a href="{{ url('auth/oauth/redirectToProvider/github') }}"><img src="{{ asset('images/home/github-login.jpg') }}" alt="github登录" title="github登录"></a>
-                    </li>
+                    {{--<li class="col-xs-6 col-md-4 col-lg-4 b-login-img">--}}
+                        {{--<a href="{{ url('auth/oauth/redirectToProvider/github') }}"><img src="{{ asset('images/home/github-login.jpg') }}" alt="github登录" title="github登录"></a>--}}
+                    {{--</li>--}}
                 </ul>
             </div>
         </div>
